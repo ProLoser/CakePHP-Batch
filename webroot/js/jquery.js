84cf87ecb5d6d164970817b68d8a94c60e868346
Dean Sofer <proloser@hotmail.com>
2011-10-23 18:51:11 -0700

    Enhanced the js behavior a tiny bit to prevent highlighting when clicking links/inputs

--- webroot/js/jquery.js
+++ webroot/js/jquery.js
@@ -1,40 +1,40 @@
 $(document).ready(function(){
-	$('form.batch').delegate('tr', 'click', function(event) {
-	    var $this = $(this);
-	    var $box = $this.find('input.batch');
-	    console.log('called');
+    $('form.batch').delegate('tr', 'click', function(event) {
+        var $this = $(this);
+        var $box = $this.find('input.batch');
 
-	    if ($box.is(':checked')) {
-	        $box.removeAttr('checked');
-	        $this.removeClass('checked');
-	    } else {
-	        $box.attr('checked', 'checked');
-	        $this.addClass('checked');
-	    }
-	}).delegate('input.batch', 'click', function(event) {
-	    event.stopPropagation();
-	    var $box = $(this);
-
-	    if ($box.is(':checked')) {
-	        $box.closest('tr').addClass('checked');
-	    } else {
-	        $box.closest('tr').removeClass('checked');
-	    }
-	});
-	$('input.batch-all').live('click', function(){
-		if ($(this).is(':checked')) {
-			$('form.batch input.batch').attr('checked','checked').closest('tr').addClass('checked');
-		} else {
-			$('form.batch input.batch').removeAttr('checked').closest('tr').removeClass('checked');
+        if ($box.is(':checked')) {
+            $box.removeAttr('checked');
+            $this.removeClass('checked');
+        } else {
+            $box.attr('checked', 'checked');
+            $this.addClass('checked');
+        }
+    }).delegate('input, a', 'click', function(event) {
+        event.stopPropagation();
+        var $el = $(this);
+		if ($el.is('input.batch')) {
+	        if ($el.is(':checked')) {
+	            $el.closest('tr').addClass('checked');
+	        } else {
+	            $el.closest('tr').removeClass('checked');
+	        }
 		}
-	});
-	$('tr.batch').delegate('input:checkbox', 'change', function() {
-		$this = $(this);
-		$el = $this.next('input, select');
-		if ($el.is(':enabled') && !$this.is(':checked')) {
-			$el.attr('disabled', 'disabled');
-		} else {
-			$el.removeAttr('disabled');
-		}
-	});
+    });
+    $('input.batch-all').live('click', function(){
+        if ($(this).is(':checked')) {
+            $('form.batch input.batch').attr('checked','checked').closest('tr').addClass('checked');
+        } else {
+            $('form.batch input.batch').removeAttr('checked').closest('tr').removeClass('checked');
+        }
+    });
+    $('tr.batch').delegate('input:checkbox', 'change', function() {
+        $this = $(this);
+        $el = $this.next('input, select');
+        if ($el.is(':enabled') && !$this.is(':checked')) {
+            $el.attr('disabled', 'disabled');
+        } else {
+            $el.removeAttr('disabled');
+        }
+    });
 });
